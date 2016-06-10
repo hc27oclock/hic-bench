72c159d3d043dd2a40c790ba0cd56b2f50041174
igor <igordot@users.noreply.github.com>
2016-06-09 22:55:40 -0400

    take care of more r packages

--- code/code.main/install-r-packages.r
+++ code/code.main/install-r-packages.r
@@ -1,10 +1,13 @@
-#!/usr/bin/Rscript
+#!/usr/bin/env Rscript
 
 write("Installing R packages...", stderr())
-for (p in c("flsa", "genlasso", "ggplot2", "optparse", "pastecs", "plotrix", "reshape2", "zoo", "plyr", "data.table", "gridExtra", "scales", "RColorBrewer", "grid", "stringr")) 
-  if (!suppressPackageStartupMessages(require(p,character.only=TRUE,quietly=TRUE,warn.conflicts=FALSE))) {
-    install.packages(p, repos='http://cran.us.r-project.org') 
-  }
 
+packages = c("flsa", "genlasso", "optparse", "ggplot2", "pastecs", "plotrix",
+	"reshape2", "zoo", "plyr", "data.table", "gridExtra", "scales", "grid",
+	"RColorBrewer", "stringr", "chron", "corrplot")
 
+for (p in packages)
+	if (!suppressPackageStartupMessages(require(p, character.only=TRUE, quietly=TRUE, warn.conflicts=FALSE))) {
+		install.packages(p, repos="http://cran.us.r-project.org")
+	}
 
