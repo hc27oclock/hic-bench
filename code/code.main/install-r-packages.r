a804a163bb66db05616b26746fc2e4a3adf4db93
Stephen K <stevekm@users.noreply.github.com>
2016-06-30 15:30:04 -0400

    Update install-r-packages.r

--- code/code.main/install-r-packages.r
+++ code/code.main/install-r-packages.r
@@ -1,13 +1,34 @@
 #!/usr/bin/env Rscript
 
-write("Installing R packages...", stderr())
+# old version :
+# packages = c("flsa", "genlasso", "optparse", "ggplot2", "pastecs", "plotrix",
+#              "reshape2", "zoo", "plyr", "data.table", "gridExtra", "scales", "grid",
+#              "RColorBrewer", "stringr", "chron", "corrplot")
+# 
+# for (p in packages)
+#   if (!suppressPackageStartupMessages(require(p, character.only=TRUE, quietly=TRUE, warn.conflicts=FALSE))) {
+#     install.packages(p, repos="http://cran.us.r-project.org")
+#   }
 
-packages = c("flsa", "genlasso", "optparse", "ggplot2", "pastecs", "plotrix",
-	"reshape2", "zoo", "plyr", "data.table", "gridExtra", "scales", "grid",
-	"RColorBrewer", "stringr", "chron", "corrplot")
+# new version:
+# Bioconductor packages
+for (package in c("ChIPseeker","TxDb.Hsapiens.UCSC.hg19.knownGene","clusterProfiler","org.Hs.eg.db","wavClusteR","DiffBind",
+                  "biomaRt","ChIPpeakAnno")) {
+  # if(package %in% rownames(installed.packages()) == FALSE){install.packages(package, repos="http://cran.us.r-project.org")}
+  if(package %in% rownames(installed.packages()) == FALSE){
+    source("https://bioconductor.org/biocLite.R")
+    biocLite(package)
+  }
+}
 
-for (p in packages)
-	if (!suppressPackageStartupMessages(require(p, character.only=TRUE, quietly=TRUE, warn.conflicts=FALSE))) {
-		install.packages(p, repos="http://cran.us.r-project.org")
-	}
-
+# R packages
+package_list<-c("UpSetR", "ggplot2", "grid", "plyr", "knitr", "VennDiagram", 
+                "gridExtra", "datasets", "digest", "Hmisc", "xtable", "reshape2", 
+                "data.table", "scales", "corrplot", "RColorBrewer", "lattice", 
+                "gplots", "MASS", "stringr", "flsa", "genlasso", "optparse", 
+                "pastecs", "plotrix", "zoo", "reshape", "chron") # 'preprocessCore'
+for (p in package_list) 
+  if (!require(p,character.only=TRUE,quietly=TRUE,warn.conflicts=FALSE)) {
+    install.packages(p,repos="http://cran.rstudio.com/") 
+    library(p,character.only=TRUE,verbose=FALSE)
+  }
