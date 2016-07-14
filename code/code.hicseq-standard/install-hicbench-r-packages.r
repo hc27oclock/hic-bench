4bf8dc9b1629e6e05158d7e66e6c05c4b92675bc
Aristotelis Tsirigos <tsirigos@gmail.com>
2016-07-14 12:33:12 -0400

    created customized install-r-scripts for hic

---
+++ code/code.hicseq-standard/install-hicbench-r-packages.r
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+write("Checking installation of R packages", stderr())
+
+
+packages = c("flsa", "genlasso", "optparse", "ggplot2", "pastecs", "plotrix",
+             "reshape2", "zoo", "plyr", "data.table", "gridExtra", "scales", "grid",
+             "RColorBrewer", "stringr", "chron", "corrplot")
+
+for (p in packages)
+  if (!suppressPackageStartupMessages(require(p, character.only=TRUE, quietly=TRUE, warn.conflicts=FALSE))) {
+    install.packages(p, repos="http://cran.us.r-project.org")
+  }
+  
+
